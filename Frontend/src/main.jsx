596bce1ce001afea7acbb75e497ceb0224f540a8
Kaushik Mandal <koushik369mondal@gmail.com>
2025-08-13 18:58:44 +0530

    update dependencies to React 19.1.1 and ReactDOM 19.1.1; refactor main.jsx for clarity

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -5,7 +5,8 @@ import App from "./App.jsx";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 
-createRoot(document.getElementById("root")).render(
+const root = createRoot(document.getElementById("root"));
+root.render(
   <BrowserRouter>
     <StrictMode>
       <App />
