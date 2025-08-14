5ead2cc18fff48c9ce50f8002b22fb8e4bcfab83
Kalpesh Sonar <143838572+sonar-kalpesh-18@users.noreply.github.com>
2025-08-14 18:30:19 +0530

    Update Settings.jsx

--- Frontend/src/components/Settings.jsx
+++ Frontend/src/components/Settings.jsx
@@ -84,11 +84,6 @@ function Settings() {
               </ListGroup.Item>
             </ListGroup>
           </Col>
-          <Col lg={4} className="text-center d-flex align-items-center">
-            <div className="w-100">
-              <img src="src\assets\settings-side-img.png" alt="Illustration" className="img-fluid" style={{ maxWidth: '400px', opacity: 0.85 }} />
-            </div>
-          </Col>
         </Row>
 
         {/* Modals */}
